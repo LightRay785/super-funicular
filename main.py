7803749ef36daab0672b32ae33603e4f588264e6
LightRay785 <98335155+LightRay785@users.noreply.github.com>
2022-08-20 00:44:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
 import pygame
-import os
 import random
 from sys import exit
 pygame.init()
 
 # Screen Setup
-width, height = 600, 1100
+width, height = 551, 630
 screen = pygame.display.set_mode((width, height))
 fps = 60
 pygame.display.set_caption('Flappy Bird')
-bird = pygame.image.load('flappy-bird.png')
+flappy_bird = pygame.image.load('birdy.png')
+sky = pygame.image.load('background.png')
+ground = pygame.image.load('groundimg.jpg')
+pipe_up = pygame.image.load('pipe-up.png')
+pipe_down = pygame.image.load('pipe-down.png')
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = flappy_bird
+        self.rect = self.image.get_rect()
+        self.rect.center = 100, 315
+        self.velocity = 0
+    def update(self, key): 
+        self.velocity += 0.5
+        if self.velocity >= 6:
+            self.vel = 7
+        if self.rect.y < 500: self.rect.y += self.velocity
+        if key[pygame.K_SPACE] and self.rect.y > 0:
+            self.velocity = -7
 
-#class Bird(pygame.sprite.Sprite):
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(self)
+        self.rect.x -= 1
+        if self.rect.x <= -width: self.kill
+
 
 clock = pygame.time.Clock()
+pygame.transform.scale(ground, (ground.get_width(), 720))
+
+bird = pygame.sprite.GroupSingle()
+bird.add(Bird())
+x = 0
+pipe = pygame.sprite.Group()
+pygame.time.delay(1000)
 while True:
-    screen.fill(255,255,255)
-    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+    screen.blit(sky, (0,0))
+    screen.blit(ground, (0, 520))
+    key = pygame.key.get_pressed()
+    pipe.draw(screen)
+    pipe.update()
+    bird.draw(screen)
+    bird.update(key)
     clock.tick(fps)
+    pygame.display.flip()
+    
+
+
 
 #WIP
